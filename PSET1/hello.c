2e814d369c70068be94741c80d404e0b427d9c61
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2020-11-01 14:38:15 -0500

    Delete hello.c

--- PSET1/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    // get_string get's input from a user.
-    string name = get_string("What's your name?\n");
-    printf("hello, %s\n", name);
-}

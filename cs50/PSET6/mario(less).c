2795dbc850a57c20cee81c2794b301b76321fd8b
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2020-11-06 16:41:03 -0500

    Create mario(less).c

---
+++ cs50/PSET6/mario(less).c
@@ -0,0 +1,13 @@
+while True:
+    height = input("Height: ")
+
+    try:
+        height = int(height)
+        if height >= 1 and height <= 8:
+            for i in range(1, height+1):
+                spaces_count = (height - i) * " "
+                hash_tags = i * "#"
+                print(spaces_count + hash_tags)
+            break
+    except ValueError:
+        pass
